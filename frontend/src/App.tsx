4acd78c52fde2304e40362c81fe721e9cd61c251
elmo <pnthaha@gmail.com>
2023-04-28 00:54:01 +0800

    feat:first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold text-blue-600">dds</h1>
+    </div>
+  );
+}
+
+export default App;
